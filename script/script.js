207a5e53c389da0dcf40c640f3deacfbb2511411
cssdominique <dominique@christiansteven.com>
2016-06-28 17:00:21 -0400

    adding example work

--- script/script.js
+++ script/script.js
@@ -15,6 +15,25 @@ var track_url = 'https://soundcloud.com/lewisbanksvoice/lewis-banks-commercial-d
 SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
   console.log('oEmbed response: ', oEmbed);
   console.log(oEmbed.html);
-  $(".how").append('<iframe width="100%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F127268567&show_artwork=true&auto_play=true"></iframe>');
-  $("iframe").css('display','none');
+  $(".commercial").append('<iframe width="100%" height="200" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F127268567&show_artwork=true&auto_play=true"></iframe>');
+  //$("iframe").css('display','none');
 });
+
+ $('ul').on('click', 'li', function(event){
+ 	console.log(this);
+ 	//console.log(this.a);
+
+ 		$("ul.navBar>li>a.active").removeClass("active");
+ 		//console.log(this.getElementsByClassName());
+ 		console.log()
+ 		$(this).closest('li').addClass("active");
+ 		$(this).closest('li a').addClass("active");
+//     var items = document.getElementsByClassName("navBar li");
+// 		//var items = ul.getElementsByTagName("li");
+// 		for (var i = 0; i < items.length; ++i) {
+//   		if ($("item[i]").hasClass("active")) {
+//   			$("item[i]").removeClass("active");
+//   }
+// }
+
+    })
