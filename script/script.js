3c6ae9df5cca2ff095781ddc8b9a7f9d656c1d93
cssdominique <dominique@christiansteven.com>
2016-06-28 14:11:46 -0400

    landingpage

---
+++ script/script.js
@@ -0,0 +1,20 @@
+$(function(){
+	function blinker() {
+    $('.blink_me').fadeOut(1000);
+    $('.blink_me').fadeIn(1000);
+}
+
+setInterval(blinker, 3000);
+});
+
+SC.initialize({
+  client_id: '95ce484b988bfe2c552dad8fe64c351f'
+});
+
+var track_url = 'https://soundcloud.com/lewisbanksvoice/lewis-banks-commercial-demo';
+SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
+  console.log('oEmbed response: ', oEmbed);
+  console.log(oEmbed.html);
+  $(".how").append('<iframe width="100%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F127268567&show_artwork=true&auto_play=true"></iframe>');
+  $("iframe").css('display','none');
+});
